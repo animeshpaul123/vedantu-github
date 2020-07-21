003d7bf66063d3f3977bf308088717c6fc1bf790
Animesh Paul <animeshpaul341@gmail.com>
2020-07-21 19:54:25 +0530

    fixes error deployment

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
-const initialState = {}
+// const initialState = {}
 const middleware = [thunk]
 const store = createStore(rootReducer, applyMiddleware(...middleware))
 export default store
