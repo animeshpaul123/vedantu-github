c984ac8ee8f52645a04c07bd23998560a64a0096
Animesh Paul <animeshpaul341@gmail.com>
2020-07-21 19:58:11 +0530

    Updates

---
+++ precache-manifest.a60b1578099eacb897e31b5683789c02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cae38184effbd58628bc04291a16155",
+    "url": "/vedantu-github/index.html"
+  },
+  {
+    "revision": "56c593cd454b7d1253ca",
+    "url": "/vedantu-github/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9ece74107256c0c4dcb2",
+    "url": "/vedantu-github/static/css/main.3f231516.chunk.css"
+  },
+  {
+    "revision": "56c593cd454b7d1253ca",
+    "url": "/vedantu-github/static/js/2.15a1b801.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/vedantu-github/static/js/2.15a1b801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ece74107256c0c4dcb2",
+    "url": "/vedantu-github/static/js/main.05629524.chunk.js"
+  },
+  {
+    "revision": "ac333b5dd3fdd1d1f3eb",
+    "url": "/vedantu-github/static/js/runtime-main.a170c05a.js"
+  }
+]);
